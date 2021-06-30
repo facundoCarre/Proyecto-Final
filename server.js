aea4de8b05ba97c33ff10962f7e3bb3e63e018cc
Facundo Carreño <fcarreno@simtlix.com>
2021-06-30 17:30:46 -0300

    eliomina middlware innesesario

--- server.js
+++ server.js
@@ -11,29 +11,9 @@ app.use(express.static('public'));
 const productosRouter = require('./routes/producto');
 const carritoRouter = require('./routes/carrito');
 
-
-carritoRouter.use((req,res,next) =>{
-
-    let body = req.body;
-    if(body.permisos.administrador && body.permisos.usuario){
-       next()
-    }else{
-        res.status(500).send('No tiene los permisos necesarios para accdeder a esta ruta')
-    }
-})
-
 app.use('/api', productosRouter);
 app.use('/api', carritoRouter);
 // pongo a escuchar el servidor en el puerto indicado
-
-carritoRouter.use((req,res,next) =>{
-    let body = req.body;
-    if(body.permisos.administrador && body.permisos.usuario){
-       next()
-    }else{
-        res.status(500).send('No tiene los permisos necesarios para accdeder a esta ruta')
-    }
-})
 const puerto = 8080;
 const server = app.listen(puerto, () => {
     console.log(`servidor escuchando en http://localhost:${puerto}`);
